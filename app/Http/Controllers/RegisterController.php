c4cbac74f7f8b73c77126ed7b5e72446b9b46273
jhon262003 <jhon.mes26@gmail.com>
2024-11-11 09:38:35 -0500

    Prueba01

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -16,7 +16,10 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        //Validación
+        //Modificar el request
+        $request->$request->add(['username' => Str::slug($request->$username)]);
+
+        //Validación en la BD 
         $request->validate([
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
@@ -27,11 +30,21 @@ class RegisterController extends Controller
         //Registrar en la BD mediante en el modelo
         User::create([
             'name' => $request->name,
-            'username' => Str::lower($request->username),
+            'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password) //encripta la contraseña
         ]);
+
+        //Autenticar un Usuario es como la variable session que usabamoss
+        //Primera Forma
+        // auth()->attempt([
+        //     'email' => $request->email,
+        //     'password' => $request->password
+        // ]);
+        //Segunda Forma
+        auth()->attempt([$request->only('email', 'password')]);
         
         //Rederigir
+        return redirect()->route('post.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('principal');
@@ -9,3 +10,5 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'crear'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/muro', [PostController::class, 'index'])->name('post.index');
